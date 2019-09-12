546e160b4fb198bed09cfafa26ada98b74a1c5ea
Matthew Adams <matthew.adams@endjin.com>
2019-09-12 11:06:43 +0100

    Initial commit of the client authentication code.

---
+++ Solutions/Corvus.Identity.ManagedServiceIdentity.ClientAuthentication/Corvus/Identity/ManagedServiceIdentity/ClientAuthentication/AzureMsiTokenSource.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="AzureMsiTokenSource.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Identity.ManagedServiceIdentity.ClientAuthentication
+{
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Services.AppAuthentication;
+    using Microsoft.Extensions.Configuration;
+
+    /// <summary>
+    /// Enables applications running in an Azure service with an associated Managed Service
+    /// Identity (MSI) to authenticate using that identity.
+    /// </summary>
+    public class AzureMsiTokenSource : IServiceIdentityTokenSource
+    {
+        private readonly IConfigurationRoot configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AzureMsiTokenSource"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration root.</param>
+        public AzureMsiTokenSource(IConfigurationRoot configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        /// <inheritdoc />
+        public Task<string> GetAccessToken(string resource)
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider(this.configuration["AzureServicesAuthConnectionString"]);
+            return azureServiceTokenProvider.GetAccessTokenAsync(resource);
+        }
+    }
+}
---
+++ Solutions/Corvus.Identity.ManagedServiceIdentity.ClientAuthentication/Microsoft/Extensions/DependencyInjection/ServiceIdentityServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="ServiceIdentityServiceCollectionExtensions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    using System.Linq;
+    using Corvus.Identity.ManagedServiceIdentity.ClientAuthentication;
+
+    /// <summary>
+    /// DI initialization for services using a Managed Service Identity (MSI).
+    /// </summary>
+    public static class ServiceIdentityServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the Workflow Engine client to a service collection.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns>The modified service collection.</returns>
+        public static IServiceCollection AddAzureMsiBasedTokenSource(
+            this IServiceCollection services)
+        {
+            if (services.Any(s => s.ImplementationType == typeof(AzureMsiTokenSource)))
+            {
+                return services;
+            }
+
+            return services.AddSingleton<IServiceIdentityTokenSource, AzureMsiTokenSource>();
+        }
+    }
+}
