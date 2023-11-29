cfca1d51dc2792249f308594ceceeb5c3f1e490f
Jessica <jessicarosehill@hotmail.co.uk>
2023-11-29 14:02:09 +0000

    Made initial list of possible certificate identification criteria.

---
+++ Solutions/Corvus.Identity.Azure/Corvus/Identity/ClientAuthentication/Azure/ClientCertificateConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace Corvus.Identity.ClientAuthentication.Azure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class ClientCertificateConfiguration
+    {
+        // Next time: Work out which of these criteria we really want to include:
+
+        // Which store e.g. user or machine.
+        // Certificate name?
+        // Thumbprint?
+        // Certificate application type.
+        // Issuer name.
+        // Subject name.
+        // Subject key name?
+        // Template name?
+    }
+}
